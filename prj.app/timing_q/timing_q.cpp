c84c25bfbee5fd95fb80ba428e8eb2ba638b105b
kikikhakiri <egorfedunin@mail.ru>
2024-05-05 22:44:59 +0300

    a template function that accepts a function pointer or lambda and measures the time it takes to execute that function

--- prj.app/timing_q/timing_q.cpp
+++ prj.app/timing_q/timing_q.cpp
@@ -5,39 +5,34 @@
 #include <chrono>
 #include <iostream>
 #include <queue>
+#include <functional>
 #include "complex/complex.hpp"
 
-int main(){
+template <typename Func, typename... Args>
+auto measureTime(Func func, Args... args) {
+  auto start = std::chrono::steady_clock::now();
+  func(std::forward<Args>(args)...);
+  auto end = std::chrono::steady_clock::now();
+
+  return std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
+}
+
+int main() {
   //Testing push() of 100 elements
   auto begin_ctor = std::chrono::steady_clock::now();
-  QueueLstT<Complex>a{};
-  for(int i = 0 ; i < 1000000; i++){
-    a.Push(Complex(i));
+  QueueLstT<int> a{};
+  for (int i = 0; i < 1000000; i++) {
+    a.Push(i);
   }
   auto end_ctor = std::chrono::steady_clock::now();
   auto ans = std::chrono::duration_cast<std::chrono::microseconds>(end_ctor - begin_ctor);
-  std::cout << "queuelstt push mln" << " "  << ans.count() << '\n';
-  begin_ctor = std::chrono::steady_clock::now();
-  std::queue<Complex>b;
-  for(int i = 0 ; i < 1000000; i++){
-    b.push(Complex(i));
-  }
-  end_ctor = std::chrono::steady_clock::now();
-  ans = std::chrono::duration_cast<std::chrono::microseconds>(end_ctor - begin_ctor);
-  std::cout << "std::queue push 10000" << " " << ans.count() << "\n";
-  //Testing pop() of 100 elements
-  begin_ctor = std::chrono::steady_clock::now();
-  for(int i = 0 ; i < 1000000; i++){
-    a.Pop();
-  }
-  end_ctor = std::chrono::steady_clock::now();
-  ans = std::chrono::duration_cast<std::chrono::microseconds>(end_ctor - begin_ctor);
-  std::cout << "queuelstt pop 10000" << " "  << ans.count() << '\n';
-  begin_ctor = std::chrono::steady_clock::now();
-  for(int i = 0 ; i < 1000000; i++){
-    b.pop();
-  }
-  end_ctor = std::chrono::steady_clock::now();
-  ans = std::chrono::duration_cast<std::chrono::microseconds>(end_ctor - begin_ctor);
-  std::cout << "std::queue pop 10000" << " " << ans.count() << "\n";
+  std::cout << "queuelstt push mln" << " " << ans.count() << '\n';
+
+  QueueLstT<int> b{};
+  auto time = measureTime([&]() {
+      for(int i = 0 ; i < 1000000; i++){
+        b.Push(i);
+      }
+  });
+  std::cout << "Time taken: " << time << " microseconds\n";
 }
